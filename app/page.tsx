b4b4d4b8baafe76d7eb52d54e56f18999fd0304a
Johnson Huang <johnsonhuang@xaregroup.com>
2025-04-08 17:49:26 +0800

    add some museums

--- app/page.tsx
+++ app/page.tsx
@@ -7,145 +7,63 @@ import {
   HoverCardTrigger,
 } from '@/components/ui/hover-card';
 
-const artGalleries = [
+type Gallery = {
+  id: number;
+  name: string;
+  address: string;
+  url: string;
+  left: number;
+  top: number;
+};
+
+const artGalleries: Gallery[] = [
   {
     id: 1,
     name: '台北市立美術館',
     address: '台北市中山區中山北路三段181號',
-    description: '台灣首座現代美術館，主要收藏與展示現代與當代藝術作品。',
     url: 'https://www.tfam.museum/',
     left: 723,
     top: 123,
   },
   {
     id: 2,
-    name: '台北當代藝術館',
-    address: '台北市大同區長安西路39號',
-    description: '專注於當代藝術的展覽與推廣。',
-    url: 'https://www.mocataipei.org.tw/',
-    left: 720,
-    top: 130,
+    name: '鳳甲美術館',
+    address: '台北市北投區大業路166號11F',
+    url: 'https://www.tfam.museum/',
+    left: 705,
+    top: 101,
   },
   {
     id: 3,
-    name: '鳳甲美術館',
-    address: '台北市北投區大業路166號',
-    description: '成立於1999年，致力於推廣當代藝術與社區文化。',
-    url: 'https://www.honggahmuseum.org.tw/',
-    left: 710,
-    top: 110,
+    name: '朱銘美術館',
+    address: '新北市金山區西勢湖2號',
+    url: 'https://www.juming.org.tw/mainssl/modules/MySpace/index.php',
+    left: 732,
+    top: 58,
   },
   {
     id: 4,
-    name: '瑩瑋藝術翡翠文化博物館',
-    address: '台北市中山區樂群二路201號',
-    description: '專門展示翡翠藝術品的博物館。',
-    url: 'https://www.museumofjadeart.com/',
-    left: 725,
-    top: 125,
+    name: '忠泰美術館',
+    address: '台北市大安區市民大道三段178號',
+    url: 'https://jam.jutfoundation.org.tw/en',
+    left: 724,
+    top: 114,
   },
   {
     id: 5,
-    name: '朱銘美術館',
-    address: '新北市金山區西勢湖2號',
-    description:
-      '由雕塑家朱銘於1999年創立，是台灣最大的戶外美術館，展示朱銘及其他藝術家的雕塑作品。',
-    url: 'https://www.juming.org.tw/',
-    left: 700,
-    top: 100,
+    name: '李澤藩美術館',
+    address: '新竹市東區林森路57號3樓',
+    url: 'http://www.tzefan.org.tw/index.html',
+    left: 523,
+    top: 207,
   },
   {
     id: 6,
-    name: '順益台灣原住民博物館',
-    address: '台北市士林區至善路二段282號',
-    description: '專門展示台灣原住民文物及歷史，於1994年開幕。',
-    url: 'https://www.museum.org.tw/',
-    left: 715,
-    top: 115,
-  },
-  {
-    id: 7,
-    name: '袖珍博物館',
-    address: '台北市中山區建國北路一段96號',
-    description: '亞洲第一家以袖珍藝術為主題的博物館，展示多達200件主題作品。',
-    url: 'https://www.mwr.org.tw/',
-    left: 720,
-    top: 128,
-  },
-  {
-    id: 8,
-    name: 'Jut Art Museum',
-    address: '台北市大安區建國南路一段178號',
-    description: '成立於2016年，專注於現代藝術與建築的展覽。',
-    url: 'https://www.jutartmuseum.org/',
-    left: 725,
-    top: 135,
-  },
-  {
-    id: 9,
-    name: '台灣設計館',
-    address: '台北市信義區光復南路133號',
-    description: '位於松山文創園區內，展示台灣設計相關作品。',
-    url: 'https://www.tdcd.org.tw/',
-    left: 730,
-    top: 140,
-  },
-  {
-    id: 10,
-    name: '李澤藩美術館',
-    address: '新竹市東區東大路一段1號',
-    description: '主要收藏與展示畫家李澤藩的作品，成立於1994年。',
-    url: 'https://www.hcccb.gov.tw/zh-tw/Content/Content?NodeID=659',
-    left: 650,
-    top: 220,
-  },
-  {
-    id: 11,
-    name: '國立台灣美術館',
+    name: '國立臺灣美術館',
     address: '台中市西區五權西路一段2號',
-    description:
-      '成立於1988年，是台灣唯一的國家級美術館，主要展示台灣現代與當代藝術作品。',
     url: 'https://www.ntmofa.gov.tw/',
-    left: 428,
-    top: 396,
-  },
-  {
-    id: 12,
-    name: '亞洲大學現代美術館',
-    address: '台中市霧峰區柳豐路500號',
-    description: '由建築師安藤忠雄設計，於2013年開幕，主要展示現代藝術作品。',
-    url: 'https://museum.asia.edu.tw/',
-    left: 430,
-    top: 420,
-  },
-  {
-    id: 13,
-    name: '彰化縣立美術館',
-    address: '彰化縣彰化市卦山路3號',
-    description:
-      '建築外觀採用樹枝狀的鋼柱結構，強調將自然生態融入現代建築中，展覽涵蓋國內外知名藝術家作品。',
-    url: 'https://www.chmaf.gov.tw/',
-    left: 410,
-    top: 450,
-  },
-  {
-    id: 14,
-    name: '嘉義市立美術館',
-    address: '嘉義市西區廣寧街101號',
-    description: '成立於2020年，展示多元藝術作品。',
-    url: 'https://chiayiartmuseum.chiayi.gov.tw/',
-    left: 390,
-    top: 500,
-  },
-  {
-    id: 15,
-    name: '台南市美術館',
-    address: '台南市中西區忠義路二段1號',
-    description:
-      '分為兩館，第一館為1931年建成的原台南警察署，第二館由建築師坂茂設計，於2019年開幕，致力於發掘與展示台灣在地藝術。',
-    url: 'https://www.tnam.museum/',
-    left: 370,
-    top: 600,
+    left: 431,
+    top: 397,
   },
 ];
 
@@ -174,8 +92,20 @@ export default function Home() {
               className="absolute flex items-center"
             >
               <HoverCard>
-                <HoverCardTrigger className="mr-2 h-3 w-3 rounded-full bg-red-600"></HoverCardTrigger>
-                <HoverCardContent>{gallery.name}</HoverCardContent>
+                <HoverCardTrigger className="h-[14px] w-[14px] rounded-full border bg-red-600 shadow-md" />
+                <HoverCardContent>
+                  <div className="flex flex-col">
+                    <div className="mb-2 font-semibold">{gallery.name}</div>
+                    <div className="text-sm">{gallery.address}</div>
+                    <a
+                      href={gallery.url}
+                      target="_blank"
+                      className="text-sm text-blue-800 underline"
+                    >
+                      {gallery.url}
+                    </a>
+                  </div>
+                </HoverCardContent>
               </HoverCard>
             </div>
           ))}
